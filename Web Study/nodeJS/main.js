5ff690bc522f5e122086e9995a0f33ad27d65fc2
unknown <ryulurala@gmail.com>
2020-08-09 20:49:19 +0900

    Study nodeJS develope Dynamic web app by query string

--- Web Study/nodeJS/main.js
+++ Web Study/nodeJS/main.js
@@ -1,17 +1,43 @@
 var http = require("http");
 var fs = require("fs");
+var url = require("url"); // url 모듈을 사용
+
 var app = http.createServer(function (request, response) {
-  var url = request.url;
-  if (request.url == "/") {
-    url = "/index.html";
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var title = queryData.id;
+
+  if (_url == "/") {
+    title = "Welcome";
   }
-  if (request.url == "/favicon.ico") {
-    response.writeHead(404);
-    response.end(); // 서버가 클라이언트에 의해 반응
-    return;
+  if (_url == "/favicon.ico") {
+    return response.writeHead(404);
   }
   response.writeHead(200);
-  console.log(__dirname + url);
-  response.end(fs.readFileSync(__dirname + "/public" + url)); // 파일을 읽음
+  fs.readFile(`./public/data/${title}.txt`, "utf-8", function (
+    err,
+    description
+  ) {
+    var template = `
+  <!DOCTYPE html>
+<html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    <ul>
+      <li><a href="/?id=HTML">HTML</a></li>
+      <li><a href="/?id=CSS">CSS</a></li>
+      <li><a href="/?id=JavaScript">JavaScript</a></li>
+    </ul>
+    <h2>${title}</h2>
+    ${description}
+  </body>
+</html>`;
+    console.log("title :>> ", title);
+    response.end(template);
+  });
 });
-app.listen(3000);
+app.listen(8080);
