6e0dca2cfcedb464ee338c0af686fb3414e32595
unknown <ryulurala@gmail.com>
2020-08-12 19:33:08 +0900

    reorganize directory

--- Server Study/GameServer/Common/Packet/GenPackets.bat
+++
@@ -1,3 +0,0 @@
-START ../../PacketGenerator/bin/Debug/PacketGenerator.exe ../../PacketGenerator/PDL.xml
-XCOPY /Y GenPackets.cs "../../DummyClient/Packet"
-XCOPY /Y GenPackets.cs "../../Server/Packet"
