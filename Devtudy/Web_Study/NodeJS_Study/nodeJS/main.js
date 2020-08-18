5e4f77e0efd1ce18f408dcca2d04f618c6225b4f
unknown <ryulurala@gmail.com>
2020-08-18 22:48:06 +0900

    Study nodeJS (queryData)

--- Devtudy/Web_Study/NodeJS_Study/nodeJS/main.js
+++ Devtudy/Web_Study/NodeJS_Study/nodeJS/main.js
@@ -5,39 +5,70 @@ var url = require("url"); // url 모듈을 사용
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
+  var pathName = url.parse(_url, true).pathname;
   var title = queryData.id;
 
-  if (_url == "/") {
-    title = "Welcome";
+  // console.log("pathName :>> ", pathName);
+  // console.log("queryData :>> ", queryData);
+  if (pathName === "/") {
+    if (title === undefined) {
+      fs.readFile(`./public/data/${title}.txt`, "utf-8", function (
+        err,
+        description
+      ) {
+        title = "Welcome";
+        var description = "Hello, Node.js";
+        var template = `
+    <!DOCTYPE html>
+  <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8" />
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ul>
+      <h2>${title}</h2>
+      ${description}
+    </body>
+  </html>`;
+        response.writeHead(200); // 파일을 성공적으로 전송
+        response.end(template);
+      });
+    } else {
+      fs.readFile(`./public/data/${title}.txt`, "utf-8", function (
+        err,
+        description
+      ) {
+        var template = `
+    <!DOCTYPE html>
+  <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8" />
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ul>
+      <h2>${title}</h2>
+      ${description}
+    </body>
+  </html>`;
+        response.writeHead(200); // 파일을 성공적으로 전송
+        response.end(template);
+      });
+    }
+  } else {
+    response.writeHead(404); // 파일을 찾을 수 없다는 코드 전송
+    response.end("Not found!");
   }
-  if (_url == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  fs.readFile(`./public/data/${title}.txt`, "utf-8", function (
-    err,
-    description
-  ) {
-    var template = `
-  <!DOCTYPE html>
-<html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8" />
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    <ul>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ul>
-    <h2>${title}</h2>
-    ${description}
-  </body>
-</html>`;
-    console.log("title :>> ", title);
-    response.end(template);
-  });
 });
 app.listen(8080);
