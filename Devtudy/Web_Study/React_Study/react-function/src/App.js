a403505ee19e11ae8dc9291003596c3ea0f20e97
unknown <ryulurala@gmail.com>
2020-08-21 13:52:35 +0900

    Study React(function vs class) - end

--- Devtudy/Web_Study/React_Study/react-function/src/App.js
+++ Devtudy/Web_Study/React_Study/react-function/src/App.js
@@ -2,11 +2,42 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  var [funcShow, setFuncShow] = useState(true);
+  var [classShow, setClassShow] = useState(true);
   return (
+    // funcShow, classShow가 true일 경우 실행
     <div className="container">
       <h1>Hello World</h1>
-      <FuncComp initNumber={2} />
-      {/* <ClassComp initNumber={2} /> */}
+      <input
+        type="button"
+        value="remove func"
+        onClick={function () {
+          setFuncShow(false);
+        }}
+      />
+      <input
+        type="button"
+        value="remove class"
+        onClick={function () {
+          setClassShow(false);
+        }}
+      />
+      <input
+        type="button"
+        value="Insert func"
+        onClick={function () {
+          setFuncShow(true);
+        }}
+      />
+      <input
+        type="button"
+        value="Insert class"
+        onClick={function () {
+          setClassShow(true);
+        }}
+      />
+      {funcShow ? <FuncComp initNumber={2} /> : null}
+      {classShow ? <ClassComp initNumber={2} /> : null}
     </div>
   );
 }
@@ -17,10 +48,27 @@ function FuncComp(props) {
   var [number, setNumber] = useState(props.initNumber);
   var [_date, setDate] = useState(new Date().toString());
 
-  useEffect(function () {
-    console.log("%cfunc => useEffect" + ++funcId, funcStyle);
-    document.title = number + ":" + _date;
-  });
+  // side effect
+  useEffect(
+    function () {
+      console.log(
+        "%cfunc => useEffect number(componentDidMount & ComponentDidUpdate)" +
+          ++funcId,
+        funcStyle
+      );
+      document.title = number; // 타이틀을 바꿈.
+      return function () {
+        // 이전에 호출된 useEffect()를 정리하는 작업(clean up)
+        console.log(
+          console.log(
+            "%cfunc => useEffect return(componentWillUnmount)" + ++funcId,
+            funcStyle
+          )
+        );
+      };
+    },
+    [number]
+  );
 
   console.log("%cfunc => render" + ++funcId, funcStyle);
   return (
@@ -69,6 +117,9 @@ class ClassComp extends React.Component {
   }
   componentDidUpdate(nextProps, nextState) {
     console.log("%cclass => componentDidUpdate", classStyle);
+  }
+  componentWillUnmount() {
+    console.log("%cclass => componentWillUnmount", classStyle);
   }
   render() {
     console.log("%cclass => render", classStyle);
