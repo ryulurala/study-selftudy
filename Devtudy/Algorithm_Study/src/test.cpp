ac6da82f6ec5e99cac2a5a6bd112dca8364bbf27
ryulurala <ryulurala@gmail.com>
2020-09-26 16:46:25 +0900

    Study <tuple> class

--- Devtudy/Algorithm_Study/src/test.cpp
+++ Devtudy/Algorithm_Study/src/test.cpp
@@ -1,12 +1,29 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <tuple>
 
 using namespace std;
 
 int main()
 {
-    int a = 125;
-    printf("%d", -a);
+    tuple<int, int, int, int> t;
+    t = make_tuple(1, 2, 3, 4);
+
+    printf("<before>\n");
+    printf("%d\n", get<0>(t));
+    printf("%d\n", get<1>(t));
+    printf("%d\n", get<2>(t));
+    printf("%d\n", get<3>(t));
+
+    get<2>(t) = 10;
+    get<3>(t) = 20;
+
+    printf("<after>\n");
+    printf("%d\n", get<0>(t));
+    printf("%d\n", get<1>(t));
+    printf("%d\n", get<2>(t));
+    printf("%d\n", get<3>(t));
+
     return 0;
 }
